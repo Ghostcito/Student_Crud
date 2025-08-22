f71e2188bb47767331e1ba4dcd8bc81798318a04
Ghostcito <jmguticasani1254@gmail.com>
2025-08-21 22:12:27 -0500

    Remove Edit Button

--- src/components/EstudianteTable.jsx
+++ src/components/EstudianteTable.jsx
@@ -1,28 +1,26 @@
-import {deleteStudent} from "../service/studentService";
+import { deleteStudent } from "../service/studentService";
 
-function EstudianteTable({  dataStudents, fetchEstudiantes }) {
-
-
+function EstudianteTable({ dataStudents, fetchEstudiantes }) {
   const eliminarEstudiante = async (id) => {
     await deleteStudent(id);
     fetchEstudiantes();
   };
   return (
-      <table>
-        <thead>
-          <tr>
-            <th >Nombre</th>
-            <th >Dirección</th>
-            <th >Teléfono</th>
-            <th >Observacion</th>
-            <th >Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataStudents.map((est) => (
-            <tr key={est.id} className="hover:bg-gray-50">
-              <td >{est.Nombre}</td>
-              {/* <td>
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Dirección</th>
+          <th>Teléfono</th>
+          <th>Observacion</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {dataStudents.map((est) => (
+          <tr key={est.id} className="hover:bg-gray-50">
+            <td>{est.Nombre}</td>
+            {/* <td>
                     {editando === estudiante.id ? (
                       <input
                         type="text"
@@ -33,22 +31,21 @@ function EstudianteTable({  dataStudents, fetchEstudiantes }) {
                       estudiante.nombre
                     )}
                   </td> */}
-              <td >{est.Direccion}</td>
-              <td >{est.Telefono}</td>
-              <td >{est.Observacion}</td>
-              <td className="actions">
-                 <button className="edit-btn">✏️ Editar</button>
-                <button
-                  onClick={() => eliminarEstudiante(est.id)}
-                  className="delete-btn"
-                >
-                  🗑️ Eliminar
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            <td>{est.Direccion}</td>
+            <td>{est.Telefono}</td>
+            <td>{est.Observacion}</td>
+            <td className="actions">
+              <button
+                onClick={() => eliminarEstudiante(est.id)}
+                className="delete-btn"
+              >
+                🗑️ Eliminar
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
